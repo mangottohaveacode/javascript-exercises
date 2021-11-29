7551ca28d1722cc8189fe803beb5974140d76df9
mangottohaveacode <mbandic@protonmail.com>
2021-11-29 13:21:13 +0100

    done 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newTemp = ((temp-32) * 0.5556);
+  let withDec = Math.round(newTemp * 10) / 10
+  return withDec;
 };
 
-const ctof = function() {
+const ctof = function(temp) {
 
+  let newTemp = ((temp * 1.8) +32);
+  let withDec = Math.round(newTemp * 10) / 10
+  return withDec;
 };
+
+console.log(ftoc(100));
+console.log(ctof(-10));
 
 // Do not edit below this line
 module.exports = {
