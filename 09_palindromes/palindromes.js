8ad004e85c8c7c36f76abb80015bde9ecece5f09
mangottohaveacode <mbandic@protonmail.com>
2021-12-18 21:52:34 +0100

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (someString) { //Some things to say
+    let removeWhitespace = someString.replace(/\W|_/g, '');
+    let reversedString = "";
+    let isPalindrome = false;
+    for(let i=removeWhitespace.length-1; i>=0;i--){
+        reversedString = reversedString + removeWhitespace[i];
+    }
+    if(reversedString.toLowerCase() === removeWhitespace.toLowerCase()){
+        isPalindrome = true;
+    }else{
+        isPalindrome;
+    }
 
+    return isPalindrome;
 };
+
+console.log(palindromes("A car, a man, a maraca"));
 
 // Do not edit below this line
 module.exports = palindromes;
