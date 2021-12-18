6f509777add3110630c720da6035e551b4352aa3
mangottohaveacode <mbandic@protonmail.com>
2021-12-18 20:48:42 +0100

    DOne

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(number1,number2) {
+	return number1+number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1,number2) {
+	return number1-number2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for(let i=0; i<array.length;i++){
+    sum = sum + array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) { //5,3,4
+  if(array.length === 0) 
+    return 0;
 
+  let product = 1;
+
+  for (let i = 0; i < array.length; i++) {
+    product = product * array[i];
+  }
+  
+  return product;
 };
 
-const power = function() {
-	
+
+console.log(multiply([2,8,3]));
+
+const power = function(base,exponent) {
+  //expect(calculator.power(4,4)).toBe(256); // 4 to 4 power is 256
+  let result = 1;
+	for(let i=0; i<exponent;i++){
+    result = result * base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+	for(let i=1; i<=number;i++){
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
