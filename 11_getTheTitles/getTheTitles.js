2cc11731f9a28ad91156e552eb19968cf1bd72d0
mangottohaveacode <mbandic@protonmail.com>
2021-12-21 17:28:09 +0100

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ];
 
+
+const getTheTitles = function() {
+    return books.map(titles=> titles.title)
 };
+
+console.log(getTheTitles());
 
 // Do not edit below this line
 module.exports = getTheTitles;
