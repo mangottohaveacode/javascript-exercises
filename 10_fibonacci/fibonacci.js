2e1f560cea85cdbd8c73281e8332f6785a2f7269
mangottohaveacode <mbandic@protonmail.com>
2021-12-21 10:48:59 +0100

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(indexIWant) {
+    let n1 = 0;
+    let n2 = 1;
+    let nextNumber = 0;
+    let myArray = [n1,n2]
+    for(let i=1; i<100;i++){
+        nextNumber = n1 + n2;
+        myArray.push(nextNumber);
+        n1 = n2;
+        n2 = nextNumber;
+    }
+    return myArray[indexIWant];
 };
+
+console.log(fibonacci(9));
 
 // Do not edit below this line
 module.exports = fibonacci;
